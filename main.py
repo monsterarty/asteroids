3392d4daf13eb8b25b2cd7ecde2c2a5ab5b01f25
monsterarty <masleanco@gmail.com>
2024-11-26 15:10:54 +0100

    3: Added fps limit to 60 and saved to dt

--- main.py
+++ main.py
@@ -10,13 +10,17 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
     while (1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        print("A")
-        screen.fill(color=(255,255,255))
-        #pygame.display.flip()
+        screen.fill("black")
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
