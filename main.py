87c57070913242a848e493845561d5743ef40a38
monsterarty <masleanco@gmail.com>
2024-11-29 15:08:30 +0100

    10: Added collision player vs asteroids

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -47,6 +48,10 @@ def main():
 
         for obj in drawable:
             obj.draw(screen)
+
+        for asteroid in asteroids:
+            if player.collisions(asteroid):
+                raise SystemExit("Game over!")
 
         pygame.display.flip()
 
